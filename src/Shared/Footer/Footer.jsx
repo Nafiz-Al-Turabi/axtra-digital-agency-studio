d70b07d99a79a8e9e2d1172cb74ac8c0c4d1559c
Nafiz Al Turabi <nafizalturabi18cse@gmail.com>
2024-08-06 03:59:45 +0600

    Footer UI added

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -1,9 +1,85 @@
 import React from 'react';
+import logo from './../../assets/Images/logo-white.webp'
+import './Footer.css'
 
 const Footer = () => {
     return (
-        <div>
-            This is Footer
+        <div className=' bg-[#121212] text-white '>
+            <div className='flex'>
+                <div className=' px-12 py-24 border-r border-b '>
+                    <div>
+                        <img src={logo} alt="Logo" className='h-8' />
+                    </div>
+                    <p className='w-96 pt-10'>When do they work well, and when do they on us and finally, when do we actually need how can we avoid them.</p>
+                </div>
+                <div className='flex flex-col '>
+                    <a href="" className='text-xl text-center px-6 py-8  uppercase border-b hover:bg-white duration-500 hover:text-black'>Facebook</a>
+                    <a href="" className='text-xl text-center px-6 py-8  uppercase border-b hover:bg-white duration-500 hover:text-black'>Twitter</a>
+                    <a href="" className='text-xl text-center px-6 py-8  uppercase border-b hover:bg-white duration-500 hover:text-black'>Linkedin</a>
+                    <a href="" className='text-xl text-center px-6 py-8  uppercase border-b hover:bg-white duration-500 hover:text-black'>Instagram</a>
+                </div>
+                <div className='border-l border-b flex justify-center items-center w-full'>
+                    <h1 className='jelly-bounce text-[164px] uppercase '>
+                        <h1 class="text-[164px] uppercase">
+                            <span class="letter">L</span>
+                            <span class="letter">e</span>
+                            <span class="letter">t</span>
+                            <span class="letter">'</span>
+                            <span class="letter">s</span>
+                            <span class="letter mx-5"> </span>
+                            <span class="letter">T</span>
+                            <span class="letter">a</span>
+                            <span class="letter">l</span>
+                            <span class="letter">k</span>
+                        </h1>
+                    </h1>
+                </div>
+            </div>
+            <div className='flex justify-between py-16 px-12'>
+                <p>© 2022 - 2025 | Alrights reserved by Wealcoder</p>
+                <ul className='flex gap-10'>
+                    <li className="group relative overflow-hidden">
+                        <a href="#" className="block py-2 tracking-tight pr-4 pl-3 text-base text-white rounded md:p-0 uppercase">
+                            <span className="block transition-transform duration-500 ease-in-out transform group-hover:-translate-y-full">
+                                About us
+                            </span>
+                            <span className="absolute inset-0 block transition-all duration-500 ease-in-out transform translate-y-full group-hover:translate-y-0 group-hover:opacity-100 opacity-0">
+                                About us
+                            </span>
+                        </a>
+                    </li>
+                    <li className="group relative overflow-hidden">
+                        <a href="#" className="block py-2 tracking-tight pr-4 pl-3 text-base text-white rounded md:p-0 uppercase">
+                            <span className="block transition-transform duration-500 ease-in-out transform group-hover:-translate-y-full">
+                                Contact
+                            </span>
+                            <span className="absolute inset-0 block transition-all duration-500 ease-in-out transform translate-y-full group-hover:translate-y-0 group-hover:opacity-100 opacity-0">
+                                Contact
+                            </span>
+                        </a>
+                    </li>
+                    <li className="group relative overflow-hidden">
+                        <a href="#" className="block py-2 tracking-tight pr-4 pl-3 text-base text-white rounded md:p-0 uppercase">
+                            <span className="block transition-transform duration-500 ease-in-out transform group-hover:-translate-y-full">
+                                career
+                            </span>
+                            <span className="absolute inset-0 block transition-all duration-500 ease-in-out transform translate-y-full group-hover:translate-y-0 group-hover:opacity-100 opacity-0">
+                                career
+                            </span>
+                        </a>
+                    </li>
+                    <li className="group relative overflow-hidden">
+                        <a href="#" className="block py-2 tracking-tight pr-4 pl-3 text-base text-white rounded md:p-0 uppercase">
+                            <span className="block transition-transform duration-500 ease-in-out transform group-hover:-translate-y-full">
+                                faqs
+                            </span>
+                            <span className="absolute inset-0 block transition-all duration-500 ease-in-out transform translate-y-full group-hover:translate-y-0 group-hover:opacity-100 opacity-0">
+                                faqs
+                            </span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </div>
     );
 };
--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -41,7 +41,7 @@ const Navbar = () => {
               <input
                 type="text"
                 placeholder="Search"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-60 p-2.5 ml-2  "
+                className="bg-gray-50 border border-gray-500 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-60 p-2.5 ml-2  "
               />
             </div>
           )}
@@ -57,70 +57,70 @@ const Navbar = () => {
           <ul className="flex flex-col gap-10 p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white">
             <li className="group relative overflow-hidden">
               <a href="#" className="block py-2 tracking-tight pr-4 pl-3 text-base text-gray-700 rounded md:p-0 uppercase">
-                <span className="block transition-transform duration-300 ease-in-out transform group-hover:-translate-y-full">
+                <span className="block transition-transform duration-500 ease-in-out transform group-hover:-translate-y-full">
                   Home
                 </span>
-                <span className="absolute inset-0 block transition-all duration-300 ease-in-out transform translate-y-full group-hover:translate-y-0 group-hover:opacity-100 opacity-0">
+                <span className="absolute inset-0 block transition-all duration-500 ease-in-out transform translate-y-full group-hover:translate-y-0 group-hover:opacity-100 opacity-0">
                   Home
                 </span>
               </a>
             </li>
             <li className="group relative overflow-hidden">
               <a href="#" className="block py-2 tracking-tight pr-4 pl-3 text-base text-gray-700 rounded md:p-0 uppercase">
-                <span className="block transition-transform duration-300 ease-in-out transform group-hover:-translate-y-full">
+                <span className="block transition-transform duration-500 ease-in-out transform group-hover:-translate-y-full">
                   About
                 </span>
-                <span className="absolute inset-0 block transition-all duration-300 ease-in-out transform translate-y-full group-hover:translate-y-0 group-hover:opacity-100 opacity-0">
+                <span className="absolute inset-0 block transition-all duration-500 ease-in-out transform translate-y-full group-hover:translate-y-0 group-hover:opacity-100 opacity-0">
                   About
                 </span>
               </a>
             </li>
             <li className="group relative overflow-hidden">
               <a href="#" className="block py-2 tracking-tight pr-4 pl-3 text-base text-gray-700 rounded md:p-0 uppercase">
-                <span className="block transition-transform duration-300 ease-in-out transform group-hover:-translate-y-full">
+                <span className="block transition-transform duration-500 ease-in-out transform group-hover:-translate-y-full">
                   Pages
                 </span>
-                <span className="absolute inset-0 block transition-all duration-300 ease-in-out transform translate-y-full group-hover:translate-y-0 group-hover:opacity-100 opacity-0">
+                <span className="absolute inset-0 block transition-all duration-500 ease-in-out transform translate-y-full group-hover:translate-y-0 group-hover:opacity-100 opacity-0">
                   Pages
                 </span>
               </a>
             </li>
             <li className="group relative overflow-hidden">
               <a href="#" className="block py-2 tracking-tight pr-4 pl-3 text-base text-gray-700 rounded md:p-0 uppercase">
-                <span className="block transition-transform duration-300 ease-in-out transform group-hover:-translate-y-full">
+                <span className="block transition-transform duration-500 ease-in-out transform group-hover:-translate-y-full">
                   Services
                 </span>
-                <span className="absolute inset-0 block transition-all duration-300 ease-in-out transform translate-y-full group-hover:translate-y-0 group-hover:opacity-100 opacity-0">
+                <span className="absolute inset-0 block transition-all duration-500 ease-in-out transform translate-y-full group-hover:translate-y-0 group-hover:opacity-100 opacity-0">
                   Services
                 </span>
               </a>
             </li>
             <li className="group relative overflow-hidden">
               <a href="#" className="block py-2 tracking-tight pr-4 pl-3 text-base text-gray-700 rounded md:p-0 uppercase">
-                <span className="block transition-transform duration-300 ease-in-out transform group-hover:-translate-y-full">
+                <span className="block transition-transform duration-500 ease-in-out transform group-hover:-translate-y-full">
                   Team
                 </span>
-                <span className="absolute inset-0 block transition-all duration-300 ease-in-out transform translate-y-full group-hover:translate-y-0 group-hover:opacity-100 opacity-0">
+                <span className="absolute inset-0 block transition-all duration-500 ease-in-out transform translate-y-full group-hover:translate-y-0 group-hover:opacity-100 opacity-0">
                   Team
                 </span>
               </a>
             </li>
             <li className="group relative overflow-hidden">
               <a href="#" className="block py-2 tracking-tight pr-4 pl-3 text-base text-gray-700 rounded md:p-0 uppercase">
-                <span className="block transition-transform duration-300 ease-in-out transform group-hover:-translate-y-full">
+                <span className="block transition-transform duration-500 ease-in-out transform group-hover:-translate-y-full">
                   Blog
                 </span>
-                <span className="absolute inset-0 block transition-all duration-300 ease-in-out transform translate-y-full group-hover:translate-y-0 group-hover:opacity-100 opacity-0">
+                <span className="absolute inset-0 block transition-all duration-500 ease-in-out transform translate-y-full group-hover:translate-y-0 group-hover:opacity-100 opacity-0">
                   Blog
                 </span>
               </a>
             </li>
             <li className="group relative overflow-hidden">
               <a href="#" className="block py-2 tracking-tight pr-4 pl-3 text-base text-gray-700 rounded md:p-0 uppercase">
-                <span className="block transition-transform duration-300 ease-in-out transform group-hover:-translate-y-full">
+                <span className="block transition-transform duration-500 ease-in-out transform group-hover:-translate-y-full">
                   Contact
                 </span>
-                <span className="absolute inset-0 block transition-all duration-300 ease-in-out transform translate-y-full group-hover:translate-y-0 group-hover:opacity-100 opacity-0">
+                <span className="absolute inset-0 block transition-all duration-500 ease-in-out transform translate-y-full group-hover:translate-y-0 group-hover:opacity-100 opacity-0">
                   Contact
                 </span>
               </a>
