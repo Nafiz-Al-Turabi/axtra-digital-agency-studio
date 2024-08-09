1f200b03dc9c8752200d6be701ad9006003591e4
Nafiz Al Turabi <nafizalturabi18cse@gmail.com>
2024-08-10 03:10:44 +0600

    aos Animation added

--- src/Components/Brands/Brands.jsx
+++ src/Components/Brands/Brands.jsx
@@ -7,6 +7,9 @@ import brand4 from './../../assets/Images/4.webp';
 import brand5 from './../../assets/Images/5.webp';
 import brand6 from './../../assets/Images/6.webp';
 import { useTheme } from '../../Provider/ThemeContext';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+AOS.init();
 
 const Brands = () => {
     const { isDarkMode } = useTheme()
@@ -14,22 +17,64 @@ const Brands = () => {
         <div className=' max-w-7xl mx-auto py-10 md:py-0 2xl:py-20'>
             <h1 className='text-center text-xl md:text-2xl my-10 uppercase'>We worked with global largest brands</h1>
             <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6 gap-10 ml-12 md:ml-16 lg:ml-20 2xl:ml-0 '>
-                <div className='flex items-center'>
+                <div
+                    data-aos="fade-right"
+                    data-aos-offset="200"
+                    data-aos-delay="10"
+                    data-aos-duration="200"
+                    data-aos-easing="ease-in-out"
+                    data-aos-once="true"
+                    className='flex items-center'>
                     <img src={brand} alt="Brand Image" />
                 </div>
-                <div className='flex items-center'>
+                <div
+                    data-aos="fade-right"
+                    data-aos-offset="200"
+                    data-aos-delay="10"
+                    data-aos-duration="400"
+                    data-aos-easing="ease-in-out"
+                    data-aos-once="true"
+                    className='flex items-center'>
                     <img src={brand2} alt="Brand Image" />
                 </div>
-                <div className='flex items-center'>
+                <div
+                    data-aos="fade-right"
+                    data-aos-offset="200"
+                    data-aos-delay="10"
+                    data-aos-duration="600"
+                    data-aos-easing="ease-in-out"
+                    data-aos-once="true"
+                    className='flex items-center'>
                     <img src={brand3} alt="Brand Image" />
                 </div>
-                <div className='flex items-center'>
+                <div
+                    data-aos="fade-right"
+                    data-aos-offset="200"
+                    data-aos-delay="10"
+                    data-aos-duration="800"
+                    data-aos-easing="ease-in-out"
+                    data-aos-once="true"
+                    className='flex items-center'>
                     <img src={brand4} alt="Brand Image" />
                 </div>
-                <div className='flex items-center'>
+                <div
+                    data-aos="fade-right"
+                    data-aos-offset="200"
+                    data-aos-delay="10"
+                    data-aos-duration="1000"
+                    data-aos-easing="ease-in-out"
+                    data-aos-once="true"
+                    className='flex items-center'>
                     <img src={brand5} alt="Brand Image" />
                 </div>
-                <div className='flex items-center'>
+                <div
+                    data-aos="fade-right"
+                    data-aos-offset="200"
+                    data-aos-delay="10"
+                    data-aos-duration="1200"
+                    data-aos-easing="ease-in-out"
+                    data-aos-once="true"
+                    className='flex items-center'>
                     <img src={brand6} alt="Brand Image" />
                 </div>
             </div>
--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
 import { MdArrowOutward } from 'react-icons/md';
 import { useTheme } from '../../Provider/ThemeContext';
-import './Services.css'
+import './Services.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+AOS.init();
 
 const Services = () => {
     const [hoverPosition, setHoverPosition] = useState({ x: '50%', y: '50%' });
     const [isHovered, setIsHovered] = useState(false);
-    const {isDarkMode}=useTheme();
+    const { isDarkMode } = useTheme();
 
     // for Hover effect on button
     const handleMouseEnter = (e) => {
@@ -25,11 +28,26 @@ const Services = () => {
         <div className='py-20 md:py-56 relative'>
             <div className='max-w-4xl mx-auto px-4'>
                 <hr className='absolute top-20 left-1/3 w-40 rotate-90 hidden md:block' />
-                <h3 className='text-gray-500 font-bold uppercase mb-5 md:text-left'>Services</h3>
-                <h1 className='text-4xl font-bold uppercase md:text-7xl md:text-left'>Our Marketing Services</h1>
+                <div data-aos="fade-up"
+                    data-aos-offset="200"
+                    data-aos-delay="5"
+                    data-aos-duration="500"
+                    data-aos-easing="ease-in-out"
+                    data-aos-once="true"
+                >
+                    <h3 className='text-gray-500 font-bold uppercase mb-5 md:text-left'>Services</h3>
+                    <h1 className='text-4xl font-bold uppercase md:text-7xl md:text-left'>Our Marketing Services</h1>
+                </div>
                 <div className='mt-10 md:mt-20 flex flex-col md:flex-row justify-center items-center'>
                     <hr className='hidden md:block absolute left-1/3 rotate-90 w-40' />
-                    <p className='w-full md:w-96 md:text-left mt-5 md:mt-0 md:ml-96'>
+                    <p
+                        data-aos="fade-up"
+                        data-aos-offset="200"
+                        data-aos-delay="5"
+                        data-aos-duration="600"
+                        data-aos-easing="ease-in-out"
+                        data-aos-once="true"
+                        className='w-full md:w-96 md:text-left mt-5 md:mt-0 md:ml-96'>
                         Consumers today rely heavily on digital means to research products. We research a brand of bldend
                         engaging with it, according to the meanwhile, 51% of consumers say they use Google to research
                         products before buying.
@@ -38,8 +56,22 @@ const Services = () => {
             </div>
             <div className='max-w-7xl mx-auto mt-20'>
                 <div className='md:flex justify-between gap-6 md:border-t py-16 px-4 space-y-4 md:space-y-0'>
-                    <h1 className='2xl:w-1/4 text-4xl md:text-3xl 2xl:text-4xl font-bold uppercase '>Search Engine optimization</h1>
-                    <div className='md:w-1/2 md:pl-10 2xl:pl-20'>
+                    <h1
+                        data-aos="fade-right"
+                        data-aos-offset="200"
+                        data-aos-delay="5"
+                        data-aos-duration="400"
+                        data-aos-easing="ease-in-out"
+                        data-aos-once="true"
+                        className='2xl:w-1/4 text-4xl md:text-3xl 2xl:text-4xl font-bold uppercase '>Search Engine optimization</h1>
+                    <div
+                        data-aos="fade-right"
+                        data-aos-offset="200"
+                        data-aos-delay="5"
+                        data-aos-duration="600"
+                        data-aos-easing="ease-in-out"
+                        data-aos-once="true"
+                        className='md:w-1/2 md:pl-10 2xl:pl-20'>
                         <p className='md:w-96 md:text-lg font-bold'>We help brands stand out through aweful, elegant visual design. Our design mainly philosophy.</p>
                         <div className='font-bold mt-5'>
                             <p>+ Logo Design</p>
@@ -47,7 +79,14 @@ const Services = () => {
                             <p>+ Promotion</p>
                         </div>
                     </div>
-                    <div className='w-1/4'>
+                    <div
+                        data-aos="fade-right"
+                        data-aos-offset="200"
+                        data-aos-delay="5"
+                        data-aos-duration="800"
+                        data-aos-easing="ease-in-out"
+                        data-aos-once="true"
+                        className='w-1/4'>
                         <button
                             className='h-40 w-40 border md:float-right border-slate-500 rounded-full relative overflow-hidden hover:text-black duration-1000 ease-in-out mt-10 md:mt-0'
                             onMouseEnter={handleMouseEnter}
@@ -66,8 +105,22 @@ const Services = () => {
                     </div>
                 </div>
                 <div className='md:flex justify-between gap-6 md:border-t py-16 px-4 space-y-4 md:space-y-0'>
-                    <h1 className='2xl:w-1/4 text-4xl md:text-3xl 2xl:text-4xl font-bold uppercase '>Email Marketing</h1>
-                    <div className='md:w-1/2 md:pl-10 2xl:pl-20'>
+                    <h1
+                        data-aos="fade-right"
+                        data-aos-offset="200"
+                        data-aos-delay="5"
+                        data-aos-duration="400"
+                        data-aos-easing="ease-in-out"
+                        data-aos-once="true"
+                        className='2xl:w-1/4 text-4xl md:text-3xl 2xl:text-4xl font-bold uppercase '>Email Marketing</h1>
+                    <div
+                        data-aos="fade-right"
+                        data-aos-offset="200"
+                        data-aos-delay="5"
+                        data-aos-duration="600"
+                        data-aos-easing="ease-in-out"
+                        data-aos-once="true"
+                        className='md:w-1/2 md:pl-10 2xl:pl-20'>
                         <p className='md:w-96 md:text-lg font-bold'>We help brands stand out through aweful, elegant visual design. Our design mainly philosophy.</p>
                         <div className='font-bold mt-5'>
                             <p>+ Logo Design</p>
@@ -75,7 +128,14 @@ const Services = () => {
                             <p>+ Promotion</p>
                         </div>
                     </div>
-                    <div className='w-1/4'>
+                    <div
+                        data-aos="fade-right"
+                        data-aos-offset="200"
+                        data-aos-delay="5"
+                        data-aos-duration="800"
+                        data-aos-easing="ease-in-out"
+                        data-aos-once="true"
+                        className='w-1/4'>
                         <button
                             className='h-40 w-40 border md:float-right border-slate-500 rounded-full relative overflow-hidden hover:text-black duration-1000 ease-in-out mt-10 md:mt-0'
                             onMouseEnter={handleMouseEnter}
@@ -94,17 +154,39 @@ const Services = () => {
                     </div>
                 </div>
                 <div className='md:flex justify-between gap-6 md:border-t py-16 px-4 space-y-4 md:space-y-0'>
-                    <h1 className='2xl:w-1/4 text-4xl md:text-3xl 2xl:text-4xl font-bold uppercase '>Content
-                    Marketing</h1>
-                    <div className='md:w-1/2 md:pl-10 2xl:pl-20'>
-                        <p className='md:w-96 md:text-lg font-bold'>We help brands stand out through aweful, elegant visual design. Our design mainly philosophy.</p>
+                    <h1
+                        data-aos="fade-right"
+                        data-aos-offset="200"
+                        data-aos-delay="5"
+                        data-aos-duration="400"
+                        data-aos-easing="ease-in-out"
+                        data-aos-once="true"
+                        className='2xl:w-1/4 text-4xl md:text-3xl 2xl:text-4xl font-bold uppercase '>Content
+                        Marketing</h1>
+                    <div
+                        data-aos="fade-right"
+                        data-aos-offset="200"
+                        data-aos-delay="5"
+                        data-aos-duration="600"
+                        data-aos-easing="ease-in-out"
+                        data-aos-once="true"
+                        className='md:w-1/2 md:pl-10 2xl:pl-20'>
+                        <p
+                            className='md:w-96 md:text-lg font-bold'>We help brands stand out through aweful, elegant visual design. Our design mainly philosophy.</p>
                         <div className='font-bold mt-5'>
                             <p>+ Logo Design</p>
                             <p>+ Advertisement</p>
                             <p>+ Promotion</p>
                         </div>
                     </div>
-                    <div className='w-1/4'>
+                    <div
+                        data-aos="fade-right"
+                        data-aos-offset="200"
+                        data-aos-delay="5"
+                        data-aos-duration="800"
+                        data-aos-easing="ease-in-out"
+                        data-aos-once="true"
+                        className='w-1/4'>
                         <button
                             className='h-40 w-40 border md:float-right border-slate-500 rounded-full relative overflow-hidden hover:text-black duration-1000 ease-in-out mt-10 md:mt-0'
                             onMouseEnter={handleMouseEnter}
@@ -123,17 +205,39 @@ const Services = () => {
                     </div>
                 </div>
                 <div className='md:flex justify-between gap-6 md:border-t py-16 px-4 space-y-4 md:space-y-0'>
-                    <h1 className='2xl:w-1/4 text-4xl md:text-3xl 2xl:text-4xl font-bold uppercase '>Social
-                    Marketing</h1>
-                    <div className='md:w-1/2 md:pl-10 2xl:pl-20'>
-                        <p className='md:w-96 md:text-lg font-bold'>We help brands stand out through aweful, elegant visual design. Our design mainly philosophy.</p>
+                    <h1
+                        data-aos="fade-right"
+                        data-aos-offset="200"
+                        data-aos-delay="5"
+                        data-aos-duration="400"
+                        data-aos-easing="ease-in-out"
+                        data-aos-once="true"
+                        className='2xl:w-1/4 text-4xl md:text-3xl 2xl:text-4xl font-bold uppercase '>Social
+                        Marketing</h1>
+                    <div
+                        data-aos="fade-right"
+                        data-aos-offset="200"
+                        data-aos-delay="5"
+                        data-aos-duration="600"
+                        data-aos-easing="ease-in-out"
+                        data-aos-once="true"
+                        className='md:w-1/2 md:pl-10 2xl:pl-20'>
+                        <p
+                            className='md:w-96 md:text-lg font-bold'>We help brands stand out through aweful, elegant visual design. Our design mainly philosophy.</p>
                         <div className='font-bold mt-5'>
                             <p>+ Logo Design</p>
                             <p>+ Advertisement</p>
                             <p>+ Promotion</p>
                         </div>
                     </div>
-                    <div className='w-1/4'>
+                    <div
+                        data-aos="fade-right"
+                        data-aos-offset="200"
+                        data-aos-delay="5"
+                        data-aos-duration="800"
+                        data-aos-easing="ease-in-out"
+                        data-aos-once="true"
+                        className='w-1/4'>
                         <button
                             className='h-40 w-40 border md:float-right border-slate-500 rounded-full relative overflow-hidden hover:text-black duration-1000 ease-in-out mt-10 md:mt-0'
                             onMouseEnter={handleMouseEnter}
